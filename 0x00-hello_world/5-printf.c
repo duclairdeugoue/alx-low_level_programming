e0116ae2a6a3c3d3d692e50f41bbc6c95cb2b9ad
duclairdeugoue <duclairdeugoue@gmail.com>
2022-10-14 16:11:58 -0700

    Adding task 5 to project 0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints the statment given in the requirement
+ * Return: 0 if every thing is okay else a non zero value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
