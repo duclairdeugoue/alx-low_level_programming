b2582f10e3a3c93804d87b48b073e6326b85572c
duclairdeugoue <duclairdeugoue@gmail.com>
2022-10-14 16:02:23 -0700

    Fixing task 4 in project 0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle"
  * Return: 0 if compiled and run successfully else diffrent from zero
- * /
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
