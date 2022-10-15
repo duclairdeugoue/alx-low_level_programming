5fd4c31bf91bbf75a870f7250a1e112b4db90be1
duclairdeugoue <duclairdeugoue@gmail.com>
2022-10-14 17:48:30 -0700

    Fixing task 5 in project 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * prints all single digit numbers of base 10
- * followed by a new line
+ * prints all single digit numbers of base 10 starting from 0
+ * followed by a new line, using putchar
  * Return: 0 if everything is okay or not 0 else
  */
 int main(void)
 {
 	int n;
+
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
 	printf("\n");
