be7b392ea329cfeefc431e6703788542a1392102
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-10-21 20:34:23 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,8 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-  * main - Test function for positive or negative
-  * Return: 0
-  */
+#include <stdio.h>
 
-int main(void)
-{
-	int i;
+void positive_or_negative(int i);
 
-	i = 0;
-	positive_or_negative(i);
-
-return (0);
-}
+#endif /* MAIN_H */
