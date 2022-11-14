1a69bb9003c94a10e81362e0a0336164bc510fe6
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-11-14 13:40:24 +0100

    [Update]: adding solution of task dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third memeber
+ *
+ * Descrition: dog properties
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
