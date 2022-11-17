92cc2f237361c90dd97ec9843e88e56917c6d7be
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-11-17 12:09:35 +0100

    [Update]: fixing solution for task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element for which
+ * the cmp function does match and return 0 else, or -1 if no match is found
+ * or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+	int *temp_arr, flag, pos;
+
+	if (size <= 0)
+		return -1;
+	
+	flag = 0;
+	
+	for(i = 0; i < size; i++)
+	{
+		
+		if (cmp(array[i] != 0 ))
+		{
+			temp_arr[i] = array[i];
+			flag++;
+		}
+	}
+
+	if (flag != 0)
+		return temp_arr[0];
+
+	return -1;
+}
