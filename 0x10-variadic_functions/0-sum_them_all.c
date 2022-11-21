0defabd33ea2032d7fb0235a1dd1ee8128efd8bb
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-11-21 16:39:08 +0100

    [Fixed]: solving betty styles issues

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum;
+
+	if (n == 0)
+		sum = 0;
+
+	return (sum);
+}
