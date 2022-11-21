46b2fd246c8b3b73f66ad36f2b08424ec191d9db
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-11-21 16:43:26 +0100

    [Update]: adding solution of task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,10 +8,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum;
+	int i, sum = 0;
+	va_list list;
 
-	if (n == 0)
-		sum = 0;
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
 
 	return (sum);
 }
